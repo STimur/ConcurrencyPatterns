dfb3ff5d2ab3793daf16a0a7a7c9d4a276ce8702
Timur <timur.salakhetdinov@gmail.com>
2017-12-20 01:44:45 +0300

    clean Producer-Consumer sample and add random rest for Producer and Consumer to make behaviour more interesting

---
+++ src/org/geeksforgeeks/example/ProducerConsumerSample.java
@@ -0,0 +1,71 @@
+package org.geeksforgeeks.example;
+
+import java.util.LinkedList;
+
+public class ProducerConsumerSample {
+    public static void main(String[] args)
+            throws InterruptedException {
+        final PC pc = new PC();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                pc.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                pc.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+
+    public static class PC {
+        LinkedList<Integer> list = new LinkedList<>();
+        final int CAPACITY = 5;
+
+        public void produce() throws InterruptedException {
+            int value = 0;
+            while (true) {
+                synchronized (this) {
+                    while (list.size() == CAPACITY)
+                        wait();
+                    System.out.println("Producer produced-" + value);
+                    list.add(value++);
+                    notify();
+                    Thread.sleep(1000);
+                }
+                randomRest();
+            }
+        }
+
+        public void consume() throws InterruptedException {
+            while (true) {
+                synchronized (this) {
+                    while (list.size() == 0)
+                        wait();
+                    int value = list.removeFirst();
+                    System.out.println("Consumer consumed-" + value);
+                    notify();
+                    Thread.sleep(1000);
+                }
+                randomRest();
+            }
+        }
+
+        private void randomRest() throws InterruptedException {
+            if (Math.random() < 0.5)
+                Thread.sleep(1000);
+        }
+    }
+}
